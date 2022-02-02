d6892677475b7f1dc607aa4ce05f6852714c3c0c
Aren Urushiyama <s1F102003852@iniad.org>
2022-02-03 04:36:27 +0900

    ローディング画面変更

--- js/loading.js
+++ js/loading.js
@@ -1,4 +1,18 @@
+var stroke;
+stroke = new Vivus('mask', {
+    start:'manual',
+    type: 'scenario-sync',
+    duration: 30,
+    forceRender: false,
+    animTimingFunction:Vivus.EASE,
+},
+function(){
+       $("#mask").attr("class", "done");
+}
+);
+
 $(window).on('load',function(){
-    $("#loading").delay(2000).fadeOut('slow');
-    $("#loading_logo").delay(1500).fadeOut('slow');
-  });
+  $("#loading").delay(9000).fadeOut('slow');
+	$("#loading_logo").delay(9000).fadeOut('slow');
+        stroke.play();//SVGアニメーションの実行
+});
